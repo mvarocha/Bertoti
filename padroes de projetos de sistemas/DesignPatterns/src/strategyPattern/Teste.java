b8f670681c3478d58ed0206a6e8db775cd2320cf
Marcus Rocha <marcus.varocha@gmail.com>
2022-08-22 22:06:11 -0300

    update code

--- padroes de projetos de sistemas/DesignPatterns/src/strategyPattern/Teste.java
+++ padroes de projetos de sistemas/DesignPatterns/src/strategyPattern/Teste.java
@@ -4,13 +4,13 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		Veiculo corsa = new Veiculo();
+		Veiculo corsa = new Veiculo("corsa", "preto", null);
 		corsa.setDarRe(new DarReAutomatico());
 		corsa.darRe();
 		
 		System.out.println("--------------------------------");
 		
-		Veiculo biz = new Veiculo();
+		Veiculo biz = new Veiculo("biz", "branca", null);
 		biz.setDarRe(new DarReEmpurrar());
 		biz.darRe();
 
--- padroes de projetos de sistemas/DesignPatterns/src/strategyPattern/Veiculo.java
+++ padroes de projetos de sistemas/DesignPatterns/src/strategyPattern/Veiculo.java
@@ -6,12 +6,38 @@ public class Veiculo {
 	private String modelo;
 	private String cor;
 	
+	public Veiculo(String modelo, String cor, DarRe darRe) {
+		this.setModelo(modelo);
+		this.setCor(cor);
+		this.darRe = darRe;
+	}
+	
+	
+	
 	public void setDarRe(DarRe darRe) {
 		this.darRe = darRe;
 	}
 	
 	public void darRe() {
 		this.darRe.Re();
+	}
+
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
 	}
 	
 }
